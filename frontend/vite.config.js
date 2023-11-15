b0cd9a79cc0eb7f3be7eeba06b33cd714d72e1df
Eduard <routerflxv@gmail.com>
2023-11-15 22:37:10 +0100

    ds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
  plugins: [react()],
- server: {
-  port: 8081,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8081",
- },
 });
