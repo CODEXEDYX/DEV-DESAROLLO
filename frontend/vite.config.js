d14d86651c87d85ab24cceab38edf55c811a0376
Eduard <routerflxv@gmail.com>
2023-11-18 20:05:33 +0100

    dsds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import PurgeCSS from "@fullhuman/postcss-purgecss"
 export default defineConfig({
  base: "/",
  plugins: [react()],
@@ -9,4 +10,37 @@ export default defineConfig({
   host: true,
   origin: "http://0.0.0.0:8081",
  },
+
+ css: {
+	postcss: {
+		plugins: [
+			PurgeCSS({
+				content: ["index.html", "./src/**/*.jsx"],
+			}),
+		],
+	},
+},
+
+build: {
+	minify: "terser",
+	terserOptions: {
+		compress: {
+			unsafe: true,
+			reduce_vars: true,
+			join_vars: true,
+			dead_code: true,
+			if_return: true,
+			drop_console: true,
+			booleans: true,
+		},
+		mangle: {
+			//toplevel:true,
+		},
+
+		format: {
+			comments: false,
+			indent_level: 2,
+		},
+	},
+},
 });
